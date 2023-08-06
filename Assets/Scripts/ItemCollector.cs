5b05d48a516ebe0cd378b5576af02c3af9767ca0
wolfatthegate <wluo1@kent.edu>
2023-08-05 22:20:36 -0400

    I will take your cherry

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class ItemCollector : MonoBehaviour
+{
+    private int cherries = 0;
+    [SerializeField] private Text cherriesText; 
+
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            Destroy(collision.gameObject);
+            cherries ++;
+            cherriesText.text = "Cheeries: " + cherries;
+        }
+    }
+
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,29 +7,43 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private Animator anim;
-    private SpriteRenderer sprite; 
+    private SpriteRenderer sprite;
+    private BoxCollider2D boxcoll;
+
+    [SerializeField] private LayerMask jumpableGround; 
+
     private float dirX;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
+    private enum MovementState { idle, running, jumping, falling };
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        sprite = GetComponent<SpriteRenderer>(); 
+        sprite = GetComponent<SpriteRenderer>();
+        boxcoll = GetComponent<BoxCollider2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y); 
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
         // GetKeyDown is for hardcoded string like "space" or "a", "d"
         // GetButtonDown is for standardize name from Project Setting
 
+        // Wukong Jump
+        /**
         if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        **/
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -40,20 +54,39 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateAnimationState()
     {
+        MovementState state;
+
+        
         if (dirX > 0f)
         {
+            state = MovementState.running;
             sprite.flipX = false;
-            anim.SetBool("tr_running", true);
         }
         else if (dirX < 0f)
         {
-            sprite.flipX = true;
-            anim.SetBool("tr_running", true);        
+            state = MovementState.running;
+            sprite.flipX = true;     
         }
         else
         {
-            sprite.flipX = false;
-            anim.SetBool("tr_running", false);
+            state = MovementState.idle;           
         }
+        
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
+        
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(boxcoll.bounds.center, boxcoll.bounds.size, 0f, Vector2.down, .1f, jumpableGround); 
     }
 }
